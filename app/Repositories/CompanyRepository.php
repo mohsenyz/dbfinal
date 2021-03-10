65d103a108305bb2e0c4765e61472b4feb0d3974
Mohsen <mohsenyz2000@gmail.com>
2021-03-10 12:56:37 +0330

    added stats apis to company

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -15,8 +15,35 @@ class CompanyRepository extends BaseRepository
         return Company::fromQuery('select * from `companies`');
     }
 
-    public function updateCompany($data, $id) {
+    public function updateCompany($data, $id)
+    {
         return $this->update('companies', $data, "`id` = ?", [$id]);
+    }
+
+
+    public function listCompaniesWithEmployeesCount() {
+        return DB::select(<<<'TAG'
+select *, count_employees from companies join
+    (select count(*) as count_employees, company_id from employees group by company_id) as result
+    on result.company_id = companies.id
+TAG
+);
+    }
+
+
+    public function totalWorkingHoursOfCompaniesEmployeesOnFridays() {
+        return DB::select(<<<'TAG'
+        select *,
+           (
+               COALESCE((
+                   select sum(timestampdiff(HOUR, started_at, ended_at)) from timesheet_details where timesheet_id in (
+                       select timesheets.id from timesheets join employees on employees.company_id = companies.id
+                   )
+               ), 0)
+           )
+           as total_working_hours from companies
+TAG
+        );
     }
 
 
